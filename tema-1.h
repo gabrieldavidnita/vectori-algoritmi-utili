247b6a2b330ae83f6c79c8514b05d7f9a10b9d3b
Nita David Gabriel <gabriel.david.nita@gmail.com>
2026-05-11 17:08:07 +0300

    update

--- tema-1.h
+++ tema-1.h
@@ -26,22 +26,21 @@ void afisareVector1(int v[], int dim)
 
 void frecventaNumarVec1(int v[], int n,int f[])
 {
-        for(int i=0; i<n;i++)
-        {
-            f[v[i]]++;
-        }
-
+    for(int i=0; i<n; i++)
+    {
+        f[v[i]]++;
+    }
 }
 
 int celMaiMareNrDouaCifreLipsa(int v[], int n)
 {
-    int f[101]={0};
+    int f[101]= {0};
     frecventaNumarVec1(v,n,f);
-    for(int i=99;i>=10;i--)
+    for(int i=99; i>=10; i--)
     {
         if(f[i]==0)
         {
-           return i;
+            return i;
         }
     }
     return -1;
@@ -61,10 +60,10 @@ void frecventaCifreNumar(int n,int f[])
 
 void frecventaCifreVector(int v[], int n, int cif[])
 {
-    for(int i=0; i<=9;i++)
+    for(int i=0; i<=9; i++)
         cif[i]=0;
 
-    for(int i=0; i<n;i++)
+    for(int i=0; i<n; i++)
         frecventaCifreNumar(v[i],cif);
 }
 
@@ -72,10 +71,8 @@ void afisareCifreCareNuApar(int v[], int n)
 {
     int cif[10];
     int gasit=0;
-
     frecventaCifreVector(v,n,cif);
-
-    for (int i=0;i<=9;i++)
+    for (int i=0; i<=9; i++)
     {
         if (cif[i]==0)
         {
@@ -83,7 +80,6 @@ void afisareCifreCareNuApar(int v[], int n)
             gasit=1;
         }
     }
-
     if (gasit==0)
         cout<<"Toate cifrele apar in elementele vectorului";
 }
@@ -92,58 +88,46 @@ void afisareCifreCareNuApar(int v[], int n)
 
 // problema 1 e)
 
-int elementMinim(int v[], int n)
+int elementMinim(int v[],int n)
 {
-    int minim = v[0];
-
-    for (int i = 1; i < n; i++)
+    int minim=v[0];
+    for (int i=1; i<n; i++)
     {
-        if (v[i] < minim)
-            minim = v[i];
+        if (v[i]<minim)
+            minim=v[i];
     }
-
     return minim;
 }
 
 int frecventaElementMinim(int v[], int n)
 {
-    int minim = elementMinim(v, n);
-    int nr = 0;
+    int minim=elementMinim(v, n);
+    int nr=0;
 
-    for (int i = 0; i < n; i++)
+    for (int i=0; i<n; i++)
     {
-        if (v[i] == minim)
+        if (v[i]==minim)
             nr++;
     }
-
     return nr;
 }
-
-
-
 int cifraMaxima(int v[], int n)
 {
     int cif[10];
-
     frecventaCifreVector(v, n, cif);
-
-    for (int i = 9; i >= 0; i--)
+    for (int i=9; i>=0; i--)
     {
-        if (cif[i] != 0)
+        if(cif[i]!=0)
             return i;
     }
-
     return -1;
 }
 
 int frecventaCifraMaxima(int v[], int n)
 {
     int cif[10];
-
-    frecventaCifreVector(v, n, cif);
-
-    int cmax = cifraMaxima(v, n);
-
+    frecventaCifreVector(v,n,cif);
+    int cmax=cifraMaxima(v,n);
     return cif[cmax];
 }
 
@@ -151,7 +135,7 @@ int frecventaCifraMaxima(int v[], int n)
 //problema 2aa
 void frecventaCifreNumar1(int n, int f[])
 {
-     while (n!=0)
+    while (n!=0)
     {
         int uc=n%10;
         f[uc]++;
@@ -171,8 +155,6 @@ void afisareFrecventaCifre(int f[])
 void cifreMaiMultDeK(int f[], int k)
 {
     int exista=0;
-
-
     for (int i=0; i<=9; i++)
     {
         if (f[i]>k)
@@ -189,8 +171,8 @@ void cifreMaiMultDeK(int f[], int k)
 //problema 2ac
 void celMaiMicNumarCifreImpareLipsa(int f[])
 {
-    int exista = 0;
-    for (int i =1;i<=9;i=i+2)
+    int exista=0;
+    for (int i=1 ; i<=9; i=i+2)
     {
         if (f[i]==0)
         {
@@ -210,14 +192,14 @@ void celMaiMicNumarCifreImpareLipsa(int f[])
 int numarMinimCifreDeAdaugat(int f[])
 {
     int maxi=f[0];
-    for(int i=1; i<=9;i++)
+    for(int i=1; i<=9; i++)
     {
         if(f[i]>maxi)
             maxi=f[i];
     }
     int tot=0;
 
-    for(int i=0;i<=9;i++)
+    for(int i=0; i<=9; i++)
         tot=tot+(maxi-f[i]);
     return tot;
 }
